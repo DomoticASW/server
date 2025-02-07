b8f071de2df029349a52d37cd8b1029d4b24bb96
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-02-07 11:16:47 +0100

    add client submodule to serve webapp

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(express.static('client/dist'))
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
