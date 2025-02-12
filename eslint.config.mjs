e53e425b5a99908d4fae93624aaa2c17208d5be6
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-02-12 11:44:43 +0100

    chore: use "type: module" in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {ignores: ["**/dist/*", "release.config.js"]},
+  {ignores: ["**/dist/*", "release.config.cjs"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
