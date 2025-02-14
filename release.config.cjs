7b11569901e7447fa1dc8ea97ffcd46ed4ed06ef
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-02-14 11:47:13 +0100

    ci: publishing to npm

--- release.config.cjs
+++ release.config.cjs
@@ -2,5 +2,6 @@ var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     "@semantic-release/github",
     "@semantic-release/git",
+    "@semantic-release/npm",
 )
 module.exports = config
