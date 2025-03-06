b102738129c3315157462e1a1473a2051b3ae058
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-06 16:16:14 +0100

    chore: finished Types ports and implementation

--- src/ports/devices-management/Types.ts
+++ src/ports/devices-management/Types.ts
@@ -10,21 +10,6 @@ export enum Type {
 }
 
 export interface TypeConstraints<T> {
+    type: Type;
     validate(value: T): InvalidValueError | undefined;
 }
-
-export interface Enum extends TypeConstraints<string> {
-    values: Set<string>;
-}
-
-export interface IntRange extends TypeConstraints<number> {
-    min: number;
-    max: number;
-}
-
-export interface DoubleRange extends TypeConstraints<number> {
-    min: number;
-    max: number;
-}
-
-export type None<T> = TypeConstraints<T>
