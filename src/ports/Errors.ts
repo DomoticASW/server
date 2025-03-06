7482e69b89b7049bf9a417eb61bb01b1ec912e13
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-03-06 09:23:15 +0100

    chore: add interfaces to notifications package, repository package and interfaces for some errors

---
+++ src/ports/Errors.ts
@@ -0,0 +1,19 @@
+export interface DeviceNotFoundError {
+  message: string;
+}
+
+export interface InvalidTokenError {
+  message: string;
+}
+
+export interface UserNotFoundError {
+  message: string;
+}
+
+export interface DuplicateIdError {
+  message: string;
+}
+
+export interface NotFoundError {
+  message: string;
+}
