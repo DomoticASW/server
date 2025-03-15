921f8636106c819ba08986559927fa8ff2514bf5
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-03-15 13:44:33 +0100

    chore: add readonly to immutable fields

--- src/ports/scripts/ScriptRepository.ts
+++ src/ports/scripts/ScriptRepository.ts
@@ -1,4 +1,4 @@
-import { Repository } from "../repository/Repository.js";
+import { Repository } from "../Repository.js";
 import { Script, ScriptId } from "./Script.js";
 
 export type ScriptRepository = Repository<ScriptId, Script<ScriptId>>
