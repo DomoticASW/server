deff00bbae69289684d5c558cf00dcdaa3c6baf8
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-03-16 17:13:59 +0100

    refactor: use readonly where possible and use option-t library for undefined return

--- src/ports/permissions/UserDevicePermissionRepository.ts
+++ src/ports/permissions/UserDevicePermissionRepository.ts
@@ -1,4 +1,6 @@
+import { DeviceId } from "../devices-management/Device.js";
 import { Repository } from "../Repository.js"
-import { Ids, UserDevicePermission } from "./UserDevicePermission.js";
+import { UserId } from "../users-management/User.js";
+import { UserDevicePermission } from "./UserDevicePermission.js";
 
-export type UserDevicePermissionRepository = Repository<Ids, UserDevicePermission>;
+export type UserDevicePermissionRepository = Repository<[UserId, DeviceId], UserDevicePermission>;
