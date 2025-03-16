645426d1cf0520e414fe6e9742b36ea3bb109489
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-16 11:05:10 +0100

    chore: fix bad imports

--- src/ports/devices-management/DeviceGroupRepository.ts
+++ src/ports/devices-management/DeviceGroupRepository.ts
@@ -1,5 +1,4 @@
 import { Repository } from "../../ports/Repository.js";
-import { DeviceGroupId } from "../../domain/devices-management/Device.js";
-import { DeviceGroup } from "../../domain/devices-management/DeviceGroup.js";
+import { DeviceGroup, DeviceGroupId } from "../../domain/devices-management/DeviceGroup.js";
 
 export type DeviceGroupRepository = Repository<DeviceGroupId, DeviceGroup>;
