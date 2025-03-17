40ec64aea495337bb375da99ce1a1cd84b1fa537
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-17 10:38:19 +0100

    refactor: using Result type

--- src/domain/devices-management/Types.ts
+++ src/domain/devices-management/Types.ts
@@ -1,3 +1,4 @@
+import { Result } from "option-t/plain_result";
 import { InvalidValueError } from "../../ports/devices-management/Errors.js";
 import { Type } from "../../ports/devices-management/Types.js";
 import { Brand } from "../../utils/Brand.js";
@@ -6,7 +7,7 @@ export type TypeConstraints<T> = Enum | IntRange | DoubleRange | None<T>
 
 interface TypeConstraint<T> {
     readonly type: Type;
-    validate(value: T): undefined | InvalidValueError;
+    validate(value: T): Result<undefined, InvalidValueError>;
 }
 
 export interface Enum extends Brand<TypeConstraint<string>, "Enum"> {
--- src/ports/devices-management/DeviceGroupsService.ts
+++ src/ports/devices-management/DeviceGroupsService.ts
@@ -3,13 +3,14 @@ import { DeviceGroup, DeviceGroupId } from "../../domain/devices-management/Devi
 import { DeviceGroupNameAlreadyInUseError, DeviceGroupNotFoundError, DeviceNotFoundError } from "./Errors.js";
 import { InvalidTokenError, TokenError } from "../permissions/Errors.js";
 import { Token } from "../permissions/Token.js";
+import { Result } from "option-t/plain_result";
 
 export interface DeviceGroupsService {
-    addGroup(token: Token, name: string): DeviceGroupId | DeviceGroupNameAlreadyInUseError | TokenError;
-    removeGroup(token: Token, groupId: DeviceGroupId): DeviceGroupNotFoundError | TokenError | undefined;
-    renameGroup(token: Token, groupId: DeviceGroupId, name: string): DeviceGroupNotFoundError | DeviceGroupNameAlreadyInUseError | TokenError | undefined;
-    findGroup(token: Token, groupId: DeviceGroupId): DeviceGroup | DeviceGroupNotFoundError | InvalidTokenError;
-    getAllDeviceGroups(token: Token): Iterable<DeviceGroup> | InvalidTokenError;
-    addDeviceToGroup(token: Token, deviceId: DeviceId, groupId: DeviceGroupId): DeviceNotFoundError | DeviceGroupNotFoundError | TokenError | undefined;
-    removeDeviceFromGroup(token: Token, deviceId: DeviceId, groupId: DeviceGroupId): DeviceNotFoundError | DeviceGroupNotFoundError | TokenError | undefined;
+    addGroup(token: Token, name: string): Result<DeviceGroupId, DeviceGroupNameAlreadyInUseError | TokenError>;
+    removeGroup(token: Token, groupId: DeviceGroupId): Result<undefined, DeviceGroupNotFoundError | TokenError>;
+    renameGroup(token: Token, groupId: DeviceGroupId, name: string): Result<undefined, DeviceGroupNotFoundError | DeviceGroupNameAlreadyInUseError | TokenError>;
+    findGroup(token: Token, groupId: DeviceGroupId): Result<DeviceGroup, DeviceGroupNotFoundError | InvalidTokenError>;
+    getAllDeviceGroups(token: Token): Result<Iterable<DeviceGroup>, InvalidTokenError>;
+    addDeviceToGroup(token: Token, deviceId: DeviceId, groupId: DeviceGroupId): Result<undefined, DeviceNotFoundError | DeviceGroupNotFoundError | TokenError>;
+    removeDeviceFromGroup(token: Token, deviceId: DeviceId, groupId: DeviceGroupId): Result<undefined, DeviceNotFoundError | DeviceGroupNotFoundError | TokenError>;
 }
