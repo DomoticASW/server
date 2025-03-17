38cb72fffb0a7ccf3b081b62d6d72b5ac453b801
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-03-17 10:20:01 +0100

    refactor: delete files that not permit pull request

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { HTTPServerAdapter } from "./adapters/HTTPServerAdapter.js";
-import { NotificationsServiceImpl } from "./domain/notifications/NotificationService.js";
-
-
-const notificationService = new NotificationsServiceImpl()
-new HTTPServerAdapter(3000, notificationService)
--- test/domain/notifications/NotificationService.test.ts
+++
@@ -1,6 +0,0 @@
-import { NotificationsServiceImpl } from "../../../src/domain/notifications/NotificationService.js";
-
-test('adds 1 + 2 to equal 3', () => {
-    const impl = new NotificationsServiceImpl()
-    expect(impl.sendNotification("", "")).toBe(null)
-});
