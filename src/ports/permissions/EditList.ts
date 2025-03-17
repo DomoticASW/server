25253d8ba84a7569dcac3d397e0a24f50d147546
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-17 10:20:59 +0100

    fix: add missing fields on EditList and TasksList

--- src/ports/permissions/EditList.ts
+++ src/ports/permissions/EditList.ts
@@ -1,5 +1,8 @@
 import { ScriptId } from "../scripts/Script.js";
+import { UserId } from "../users-management/User.js";
 
 export interface EditList {
   readonly id: ScriptId
+
+  users: UserId[]
 }
