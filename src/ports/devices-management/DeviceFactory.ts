06af1a3a94a14615e53e59e13235d0a2dbbb2b44
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-17 10:38:19 +0100

    refactor: using Result type

--- src/ports/devices-management/DeviceFactory.ts
+++ src/ports/devices-management/DeviceFactory.ts
@@ -1,6 +1,7 @@
 import { DeviceUnreachableError } from "../../ports/devices-management/Errors.js";
 import { Device } from "../../domain/devices-management/Device.js";
+import { Result } from "option-t/plain_result";
 
 export interface DeviceFactory {
-    create(deviceUrl: URL): Device | DeviceUnreachableError;
+    create(deviceUrl: URL): Result<Device, DeviceUnreachableError>;
 }
