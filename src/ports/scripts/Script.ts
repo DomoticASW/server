a562451be14a52241705748f99b36fad80923751
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-22 11:13:42 +0100

    chore: use Brand for type ids and types

--- src/ports/scripts/Script.ts
+++ src/ports/scripts/Script.ts
@@ -1,3 +1,4 @@
+import { Brand } from "../../utils/Brand.js"
 import { Instruction } from "./Instruction.js"
 import { Trigger } from "./Trigger.js"
 
@@ -17,7 +18,7 @@ export interface Automation extends Script<AutomationId> {
   trigger: Trigger
 }
 
-export type ScriptId = string
+export type ScriptId = TaskId | AutomationId
 
-export type TaskId = ScriptId
-export type AutomationId = ScriptId
+export type TaskId = Brand<string, "TaskId">
+export type AutomationId = Brand<string, "AutomationId">
--- src/ports/users-management/User.ts
+++ src/ports/users-management/User.ts
@@ -1,12 +1,19 @@
-export type Nickname = string;
-export type Email = string;
-export type PasswordHash = string;
+import { Brand } from "../../utils/Brand.js";
+
+export type Nickname = Brand<string, "Nickname">
+export type Email = Brand<string, "Email">
+export type PasswordHash = Brand<string, "PasswordHash">
+
+export enum Role {
+    Admin = "Admin",
+    User = "User"
+}
 
 export interface User {
     nickname: Nickname;
     readonly email: Email;
     passwordHash: PasswordHash;
+    role: Role;
 }
 
-// TODO: fix
-export type Admin = User
+export type Admin = Brand<User, "Admin">
