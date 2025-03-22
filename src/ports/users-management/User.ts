51aa8e82aa277009e021497dc1e469d2d2874825
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-22 11:13:42 +0100

    chore: use Brand for type ids and types

--- src/ports/users-management/User.ts
+++ src/ports/users-management/User.ts
@@ -1,1 +1,19 @@
-export type UserId = string
+import { Brand } from "../../utils/Brand.js";
+
+export type Nickname = Brand<string, "Nickname">
+export type Email = Brand<string, "Email">
+export type PasswordHash = Brand<string, "PasswordHash">
+
+export enum Role {
+    Admin = "Admin",
+    User = "User"
+}
+
+export interface User {
+    nickname: Nickname;
+    readonly email: Email;
+    passwordHash: PasswordHash;
+    role: Role;
+}
+
+export type Admin = Brand<User, "Admin">
