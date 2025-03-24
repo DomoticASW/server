1f55c1be8b04b80f668790d5224cb7e3728b3e0c
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-24 10:10:00 +0100

    refactor: use effect library

--- src/ports/devices-management/DeviceEventsService.ts
+++ src/ports/devices-management/DeviceEventsService.ts
@@ -1,9 +1,9 @@
-import { Result } from "option-t/plain_result";
+import { Effect } from "effect/Effect";
 import { DeviceEvent, DeviceId } from "../../domain/devices-management/Device.js";
 import { DeviceNotFoundError, NotDeviceEventError } from "./Errors.js";
 
 export interface DeviceEventsService {
-    publishEvent(deviceId: DeviceId, eventName: string): Result<undefined, DeviceNotFoundError | NotDeviceEventError>;
+    publishEvent(deviceId: DeviceId, eventName: string): Effect<void, DeviceNotFoundError | NotDeviceEventError>;
     subscribeForDeviceEvents(subscriber: DeviceEventsSubscriber): void;
     unsubscribeForDeviceEvents(subscriber: DeviceEventsSubscriber): void;
 }
