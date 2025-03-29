8a0f15909edcbbaec3ed659a21a256a2b6dbdc98
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-03-29 16:08:01 +0100

    test: add test for the DeviceId creation

--- test/domain/devices-management/Device.test.ts
+++ test/domain/devices-management/Device.test.ts
@@ -1,22 +1,6 @@
-import { Device, DeviceId, DeviceStatus } from "../../../src/domain/devices-management/Device.js"
+import { DeviceId } from "../../../src/domain/devices-management/Device.js";
 
-test("DeviceId creation", () => {
-    const idValue = "1"
-    const id = DeviceId(idValue)
-    expect(id).toBe(idValue)
-})
-
-test("Device creation", () => {
-    const id = DeviceId("1")
-    const name = "Lamp"
-    const address = new URL("https://192.168.0.2:8080")
-    const status = DeviceStatus.Online
-    const d = Device(id, name, address, status, [], [], [])
-    expect(d.id).toBe(id)
-    expect(d.name).toBe(name)
-    expect(d.address).toBe(address)
-    expect(d.status).toBe(status)
-    expect(d.properties).toEqual([])
-    expect(d.actions).toEqual([])
-    expect(d.events).toEqual([])
-})
+test("DeviceId can be created", () => {
+  const deviceId = DeviceId("Id");
+  expect(deviceId).toBe("Id");
+});
