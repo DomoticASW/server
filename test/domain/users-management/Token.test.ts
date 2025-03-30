0d68cf1d0eda56a349a668fc30a7c5d50d2654e5
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-03-31 01:02:51 +0200

    fix: make Tokenimpl private

--- test/domain/users-management/Token.test.ts
+++ test/domain/users-management/Token.test.ts
@@ -1,17 +1,17 @@
 import { instanceOf } from "effect/Schema";
 import { UserRole } from "../../../src/domain/users-management/Token.js";
-import { TokenImpl } from "../../../src/domain/users-management/Token.js";
+import { Token } from "../../../src/domain/users-management/Token.js";
 import { Email } from "../../../src/domain/users-management/User.js";
 
 function createToken(email: Email = Email("ciao@gmail.com"), role: UserRole = UserRole.User) {
-  return new TokenImpl(email, role);
+    return Token(email, role);
 }
 
 test("Token creation", () => {
     const token = createToken();
     expect(token.userEmail).toBe("ciao@gmail.com");
     expect(token.role).toBe(UserRole.User);
-  })
+})
 
 test("New token generation", () => {
     const token = createToken();
