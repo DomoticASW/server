260ccd2947adb54cc5cffc1edd5d0ead8e6113aa
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-03-31 01:05:56 +0200

    feat: add DuplicateIdError and NotFoundError type

--- src/ports/Repository.ts
+++ src/ports/Repository.ts
@@ -10,6 +10,15 @@ export interface Repository<Id, Entity> {
     find(id: Id): Effect<Entity, NotFoundError>
 }
 
+
 export type DuplicateIdError = Brand<Error, "DuplicateIdError">
 
+export function DuplicateIdError(cause?: string): DuplicateIdError {
+    return { message: "Id already in use", cause: cause, __brand: "DuplicateIdError" }
+}
+
 export type NotFoundError = Brand<Error, "NotFoundError">
+
+export function NotFoundError(cause?: string): NotFoundError {
+    return { message: "Not found", cause: cause, __brand: "NotFoundError" }
+}
