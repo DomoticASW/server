397a9ebccf3c5e25223c7231f941b90f2be39c7d
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-03-30 20:36:49 +0200

    chore: test and implementation of UserDevicePermission and implementation of Email in User

--- src/domain/permissions/UserDevicePermission.ts
+++ src/domain/permissions/UserDevicePermission.ts
@@ -5,3 +5,18 @@ export interface UserDevicePermission {
   readonly email: Email
   readonly deviceId: DeviceId
 }
+
+export function UserDevicePermission(email: Email, deviceId: DeviceId): UserDevicePermission {
+  return new UserDevicePermissionImpl(email, deviceId)
+}
+
+class UserDevicePermissionImpl implements UserDevicePermission {
+
+  readonly email: Email
+  readonly deviceId: DeviceId
+
+  constructor(email: Email, deviceId: DeviceId) {
+    this.email = email
+    this.deviceId = deviceId
+  }
+}
