888d7ff07e5bdc4debe8dc2e9d81df74e3bb8dfd
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-31 09:26:32 +0200

    fix: fix some bugs in tests

--- src/ports/permissions/UserDevicePermissionRepository.ts
+++ src/ports/permissions/UserDevicePermissionRepository.ts
@@ -3,4 +3,5 @@ import { Repository } from "../Repository.js"
 import { Email } from "../../domain/users-management/User.js";
 import { UserDevicePermission } from "../../domain/permissions/UserDevicePermission.js";
 
-export type UserDevicePermissionRepository = Repository<[UserId, DeviceId], UserDevicePermission>;
+export type UserDevicePermissionRepository = Repository<[Email, DeviceId], UserDevicePermission>;
+
