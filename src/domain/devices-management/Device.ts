65b445966b06312b50569bbb6c8e1b888291604e
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-31 11:40:54 +0200

    feat: add some tests and impl of Device

--- src/domain/devices-management/Device.ts
+++ src/domain/devices-management/Device.ts
@@ -29,6 +29,29 @@ export interface Device {
     executeAction(actionId: DeviceActionId, input: unknown): Effect<void, InvalidInputError | DeviceActionError | DeviceActionNotFound>;
 }
 
+export function Device(
+    id: DeviceId,
+    name: string,
+    address: URL,
+    status: DeviceStatus,
+    properties: DeviceProperty<unknown>[],
+    actions: DeviceAction<unknown>[],
+    events: DeviceEvent[]): Device {
+    return {
+        id: id,
+        name: name,
+        address: address,
+        status: status,
+        properties: properties,
+        actions: actions,
+        events: events,
+        executeAction: function (actionId: DeviceActionId, input: unknown): Effect<void, InvalidInputError | DeviceActionError | DeviceActionNotFound> {
+            throw new Error("Function not implemented.");
+        }
+    }
+}
+
+
 export interface DeviceProperty<T> {
     readonly id: DevicePropertyId;
     readonly name: string;
