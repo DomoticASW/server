fa3f88625bc0bf67230d244c1159a832aa078c7e
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-05 10:42:01 +0200

    feat: add ScriptBuilder interface

--- src/ports/scripts/Errors.ts
+++ src/ports/scripts/Errors.ts
@@ -2,3 +2,4 @@ import { Brand } from "../../utils/Brand.js"
 import { Error } from "../Error.js"
 
 export type ScriptNotFoundError = Brand<Error, "ScriptNotFoundError">
+export type InvalidScriptError = Brand<Error, "InvalidScriptError">
