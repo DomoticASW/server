d5326e11fe59804874bddd2a13285f88c091055c
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-05 11:10:39 +0200

    feat: add of script service interface

--- src/ports/scripts/Errors.ts
+++ src/ports/scripts/Errors.ts
@@ -3,3 +3,7 @@ import { Error } from "../Error.js"
 
 export type ScriptNotFoundError = Brand<Error, "ScriptNotFoundError">
 export type InvalidScriptError = Brand<Error, "InvalidScriptError">
+export type TaskNameAlreadyInUse = Brand<Error, "TaskNameAlreadyInUse">
+export type InvalidTaskError = Brand<Error, "InvalidTaskError">
+export type AutomationNameAlreadyInUse = Brand<Error, "AutomationNameAlreadyInUse">
+export type InvalidAutomationError = Brand<Error, "InvalidAutomationError">
