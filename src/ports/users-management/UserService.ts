98ce990ab4dab75822576aa615404edc794ec6f3
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-04-06 19:59:29 +0200

    feat: add publishRegistrationRequest method implemenation

--- src/ports/users-management/UserService.ts
+++ src/ports/users-management/UserService.ts
@@ -1,7 +1,9 @@
 import { Effect } from "effect/Effect";
-import { User, Nickname, Email, PasswordHash } from "../../domain/users-management/User.js";
+import { User, Nickname, Email, PasswordHash, Role, UserImpl } from "../../domain/users-management/User.js";
 import { Token } from "../../domain/users-management/Token.js";
-import { EmailAlreadyInUseError, UserNotFoundError, TokenError, InvalidTokenError, InvalidCredentialsError, InvalidTokenFormatError } from "./Errors.js";
+import { EmailAlreadyInUseError, UserNotFoundError, TokenError, InvalidTokenError, InvalidCredentialsError, InvalidTokenFormatError, userNotFoundError } from "./Errors.js";
+import { UserRepository } from "./UserRepository.js";
+import { succeed } from "effect/Exit";
 
 export interface UsersService {
     publishRegistrationRequest(nickname: Nickname, email: Email, password: PasswordHash): Effect<void, EmailAlreadyInUseError>;
@@ -14,4 +16,33 @@ export interface UsersService {
     login(email: Email, password: PasswordHash): Effect<Token, InvalidCredentialsError>;
     verifyToken(token: Token): Effect<void, InvalidTokenError>;
     makeToken(value: string): Effect<Token, InvalidTokenFormatError>;
+}
+
+export class UsersServiceImpl implements UsersService {
+    constructor(
+        private userRepository: UserRepository,
+        private adminEmail: Email
+    ) {}
+    
+    publishRegistrationRequest(
+        nickname: Nickname, 
+        email: Email, 
+        password: PasswordHash,
+    ): Promise<Effect<void, EmailAlreadyInUseError>> {
+        try {
+            // Check if email already exists
+            const existingUser = this.userRepository.find(email);
+            if (existingUser) {
+                fail(EmailAlreadyInUseError())
+            };
+            
+            const newUser = new UserImpl(nickname, email, password, Role.User);            
+            this.userRepository.add(newUser);
+            
+            return Promise.resolve(succeed(undefined));
+        } catch (error) {
+            console.error('Error in publishRegistrationRequest:', error);
+            return fail(EmailAlreadyInUseError())
+        };
+    }
 }
