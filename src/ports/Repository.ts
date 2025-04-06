b3f34cfe34fd1c39d8815dff7e242b3ea2f13c3c
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-04-06 19:58:32 +0200

    refactor: define error functions in Errors.ts

--- src/ports/Repository.ts
+++ src/ports/Repository.ts
@@ -1,6 +1,5 @@
 import { Effect } from "effect/Effect";
-import { Brand } from "../utils/Brand.js";
-import { Error } from "./Error.js";
+import { NotFoundError, DuplicateIdError } from "./users-management/Errors.js";
 
 export interface Repository<Id, Entity> {
     add(entity: Entity): Effect<void, DuplicateIdError>
@@ -8,17 +7,4 @@ export interface Repository<Id, Entity> {
     remove(entity: Entity): Effect<void, NotFoundError>
     getAll(): Effect<Iterable<Entity>, never>;
     find(id: Id): Effect<Entity, NotFoundError>
-}
-
-
-export type DuplicateIdError = Brand<Error, "DuplicateIdError">
-
-export function DuplicateIdError(cause?: string): DuplicateIdError {
-    return { message: "Id already in use", cause: cause, __brand: "DuplicateIdError" }
-}
-
-export type NotFoundError = Brand<Error, "NotFoundError">
-
-export function NotFoundError(cause?: string): NotFoundError {
-    return { message: "Not found", cause: cause, __brand: "NotFoundError" }
 }
--- src/ports/users-management/Errors.ts
+++ src/ports/users-management/Errors.ts
@@ -11,3 +11,20 @@ export type UserNotFoundError = Brand<Error, "UserNotFoundError">
 export type TokenError = Brand<Error, "TokenError">
 export type InvalidTokenError = Brand<Error, "InvalidTokenError">
 export type UnauthorizedError = Brand<Error, "UnauthorizedError">
+
+
+export function DuplicateIdError(cause?: string): DuplicateIdError {
+    return { message: "Id already in use", cause: cause, __brand: "DuplicateIdError" }
+}
+
+export function NotFoundError(cause?: string): NotFoundError {
+    return { message: "Not found", cause: cause, __brand: "NotFoundError" }
+}
+
+export function EmailAlreadyInUseError(cause?: string): EmailAlreadyInUseError {
+    return { message: "Email already in use", cause: cause, __brand: "EmailAlreadyInUseError" }
+}
+
+export function userNotFoundError(cause?: string): UserNotFoundError {
+    return { message: "User not found", cause: cause, __brand: "UserNotFoundError" }
+}
