59acb1e3d03bc0a844b4351901d88d661e699efa
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-04-06 10:01:01 +0200

    feat: add DeviceEvent tests and impl

--- test/domain/devices-management/Device.test.ts
+++ test/domain/devices-management/Device.test.ts
@@ -1,4 +1,25 @@
-import { Device, DeviceId, DeviceStatus } from "../../../src/domain/devices-management/Device.js"
+import { Device, DeviceEvent, DeviceId, DeviceStatus, DeviceProperty, DeviceAction } from "../../../src/domain/devices-management/Device.js"
+
+interface MakeDeviceParameters {
+    id?: string,
+    name?: string,
+    address?: string,
+    status?: DeviceStatus,
+    properties?: DeviceProperty<unknown>[],
+    actions?: DeviceAction<unknown>[],
+    events?: DeviceEvent[],
+}
+function makeDevice({
+    id = "1",
+    name = "Lamp",
+    address = "https://192.168.0.2:8080",
+    status = DeviceStatus.Online,
+    properties = [],
+    actions = [],
+    events = []
+}: MakeDeviceParameters) {
+    return Device(DeviceId(id), name, new URL(address), status, properties, actions, events)
+}
 
 test("DeviceId creation", () => {
     const idValue = "1"
@@ -11,12 +32,19 @@ test("Device creation", () => {
     const name = "Lamp"
     const address = new URL("https://192.168.0.2:8080")
     const status = DeviceStatus.Online
-    const d = Device(id, name, address, status, [], [], [])
+    const events = [DeviceEvent("low battery"), DeviceEvent("full battery")]
+    const d = Device(id, name, address, status, [], [], events)
     expect(d.id).toBe(id)
     expect(d.name).toBe(name)
     expect(d.address).toBe(address)
     expect(d.status).toBe(status)
     expect(d.properties).toEqual([])
     expect(d.actions).toEqual([])
-    expect(d.events).toEqual([])
+    expect(d.events).toEqual(events)
+})
+
+test("DeviceEvent creation", () => {
+    const name = "washing done"
+    const event = DeviceEvent(name)
+    expect(event.name).toBe(name)
 })
