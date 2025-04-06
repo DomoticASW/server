056d2e9b4f50abad0499e7b0f01808f218953995
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-04-06 10:23:39 +0200

    feat: implemented device management errors

--- src/ports/devices-management/Errors.ts
+++ src/ports/devices-management/Errors.ts
@@ -2,13 +2,42 @@ import { Brand } from "../../utils/Brand.js"
 import { Error } from "../Error.js"
 
 export type InvalidValueError = Brand<Error, "InvalidValueError">
+export function InvalidValueError(cause?: string): InvalidValueError {
+    return { message: "The given value is not valid", cause: cause, __brand: "InvalidValueError" }
+}
 export type InvalidInputError = Brand<Error, "InvalidInputError">
-export type DeviceNotFound = Brand<Error, "DeviceNotFound">
+export function InvalidInputError(cause?: string): InvalidInputError {
+    return { message: "The given value is not valid a valid input", cause: cause, __brand: "InvalidInputError" }
+}
 export type DeviceActionError = Brand<Error, "DeviceActionError">
+export function DeviceActionError(cause?: string): DeviceActionError {
+    return { message: "Something went wrong while executing this action", cause: cause, __brand: "DeviceActionError" }
+}
 export type DeviceActionNotFound = Brand<Error, "DeviceActionNotFound">
+export function DeviceActionNotFound(cause?: string): DeviceActionNotFound {
+    return { message: "This device action was not found", cause: cause, __brand: "DeviceActionNotFound" }
+}
 export type DevicePropertyNotFound = Brand<Error, "DevicePropertyNotFound">
+export function DevicePropertyNotFound(cause?: string): DevicePropertyNotFound {
+    return { message: "This device property was not found", cause: cause, __brand: "DevicePropertyNotFound" }
+}
 export type DeviceGroupNameAlreadyInUseError = Brand<Error, "DeviceGroupNameAlreadyInUseError">
+export function DeviceGroupNameAlreadyInUseError(cause?: string): DeviceGroupNameAlreadyInUseError {
+    return { message: "This device group name is already used", cause: cause, __brand: "DeviceGroupNameAlreadyInUseError" }
+}
 export type DeviceGroupNotFoundError = Brand<Error, "DeviceGroupNotFoundError">
+export function DeviceGroupNotFoundError(cause?: string): DeviceGroupNotFoundError {
+    return { message: "This device group was not found", cause: cause, __brand: "DeviceGroupNotFoundError" }
+}
 export type DeviceNotFoundError = Brand<Error, "DeviceNotFoundError">
+export function DeviceNotFoundError(cause?: string): DeviceNotFoundError {
+    return { message: "This device was not found", cause: cause, __brand: "DeviceNotFoundError" }
+}
 export type DeviceUnreachableError = Brand<Error, "DeviceUnreachableError">
+export function DeviceUnreachableError(cause?: string): DeviceUnreachableError {
+    return { message: "This device was not reachable", cause: cause, __brand: "DeviceUnreachableError" }
+}
 export type NotDeviceEventError = Brand<Error, "NotDeviceEventError">
+export function NotDeviceEventError(cause?: string): NotDeviceEventError {
+    return { message: "The given event is not part of the device events", cause: cause, __brand: "NotDeviceEventError" }
+}
