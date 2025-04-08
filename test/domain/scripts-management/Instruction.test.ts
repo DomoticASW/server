2803fd5223625cfbf1fa600845274c4e5fc07151
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-08 14:07:41 +0200

    chore: add creation of start task instruction with a method

--- test/domain/scripts-management/Instruction.test.ts
+++ test/domain/scripts-management/Instruction.test.ts
@@ -1,3 +1,7 @@
-test("", () => {
-  
+import { StartTaskInstruction } from "../../../src/domain/scripts-management/InstructionImpl.js"
+import { TaskId } from "../../../src/domain/scripts-management/Script.js"
+
+test("A start task instruction can be created", () => {
+  const instruction = StartTaskInstruction(TaskId("1"))
+  expect(instruction.taskId).toBe("1")
 })
