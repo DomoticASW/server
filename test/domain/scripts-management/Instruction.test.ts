d2cb3b7ae4cb1301a616d284c61b22e4619fae22
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-08 14:18:22 +0200

    chore: add creation of CreateConstantInstruction

--- test/domain/scripts-management/Instruction.test.ts
+++ test/domain/scripts-management/Instruction.test.ts
@@ -1,7 +1,15 @@
-import { StartTaskInstruction } from "../../../src/domain/scripts-management/InstructionImpl.js"
+import { CreateConstantInstruction, StartTaskInstruction } from "../../../src/domain/scripts-management/InstructionImpl.js"
 import { TaskId } from "../../../src/domain/scripts-management/Script.js"
+import { Type } from "../../../src/ports/devices-management/Types.js"
 
 test("A start task instruction can be created", () => {
   const instruction = StartTaskInstruction(TaskId("1"))
   expect(instruction.taskId).toBe("1")
+})
+
+test("A create constant instruction can be created", () => {
+  const instruction = CreateConstantInstruction("constantName", Type.IntType, 10)
+  expect(instruction.name).toBe("constantName");
+  expect(instruction.type).toBe(Type.IntType);
+  expect(instruction.value).toBe(10);
 })
