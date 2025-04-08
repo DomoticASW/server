9b3a6416b3d13269b7a4c394cea67f6a6f2ce657
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-08 15:38:22 +0200

    chore: add number GE and number LE operators

--- src/domain/scripts-management/Operators.ts
+++ src/domain/scripts-management/Operators.ts
@@ -7,3 +7,19 @@ export function NumberEOperator(): ConditionOperator<number> {
     },
   }
 }
+
+export function NumberGEOperator(): ConditionOperator<number> {
+  return {
+    evaluate(left, right) {
+      return left.value >= right.value
+    },
+  }
+}
+
+export function NumberLEOperator(): ConditionOperator<number> {
+  return {
+    evaluate(left, right) {
+      return left.value <= right.value
+    },
+  }
+}
