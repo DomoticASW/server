18de63e57f29d806e335bf70f6b697ccdb5018ca
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-08 14:47:36 +0200

    chore: create constant instruction now adds its value to the execution environment map of constants

--- src/domain/scripts-management/InstructionImpl.ts
+++ src/domain/scripts-management/InstructionImpl.ts
@@ -1,5 +1,5 @@
 import { Type } from "../../ports/devices-management/Types.js";
-import { CreateConstantInstruction, ExecutionEnvironment, StartTaskInstruction } from "./Instruction.js";
+import { ConstantValue, CreateConstantInstruction, ExecutionEnvironment, ExecutionEnvironmentFromConstants, StartTaskInstruction } from "./Instruction.js";
 import { TaskId } from "./Script.js";
 
 export function StartTaskInstruction(taskId: TaskId): StartTaskInstruction {
@@ -18,7 +18,9 @@ export function CreateConstantInstruction<T>(name: string, type: Type, value: T)
     type: type,
     value: value,
     execute(env: ExecutionEnvironment): ExecutionEnvironment {
-      return env
+      const newEnv = ExecutionEnvironmentFromConstants(env.constants)
+      newEnv.constants.set(this, ConstantValue(value))
+      return newEnv
     }
   }
 }
