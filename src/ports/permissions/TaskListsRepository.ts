e6867a66c483494c7c23c90f581c6a0cf965914b
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-04-10 15:42:20 +0200

    refactor: fix wrong repo name

--- src/ports/permissions/TaskListsRepository.ts
+++ src/ports/permissions/TaskListsRepository.ts
@@ -2,4 +2,4 @@ import { Repository } from "../Repository.js"
 import { TaskId } from "../../domain/scripts/Script.js";
 import { TaskLists } from "../../domain/permissions/TaskLists.js";
 
-export type TaskListRepository = Repository<TaskId, TaskLists>;
+export type TaskListsRepository = Repository<TaskId, TaskLists>;
