53553a5b9555cbcca3af7c5ec06f9ae07bd80400
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-04-10 14:49:30 +0200

    chore: add color equal operator

--- src/domain/scripts-management/Operators.ts
+++ src/domain/scripts-management/Operators.ts
@@ -1,3 +1,4 @@
+import { Color } from "../devices-management/Types.js";
 import { ConditionOperator } from "./Instruction.js";
 
 export function NumberEOperator(): ConditionOperator<number> {
@@ -44,6 +45,16 @@ export function StringEOperator(): ConditionOperator<string> {
   return {
     evaluate(left, right) {
       return left.value == right.value
+    },
+  }
+}
+
+export function ColorEOperator(): ConditionOperator<Color> {
+  return {
+    evaluate(left, right) {
+      return left.value.r == right.value.r &&
+              left.value.b == right.value.b && 
+              left.value.g == right.value.g
     },
   }
 }
