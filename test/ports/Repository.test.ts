7e58236ad9cb02ec642e687f9ab82ffc6b50aab8
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-04-13 14:02:50 +0200

    fix: repositories can now deal with key uniqueness

--- test/ports/Repository.test.ts
+++ test/ports/Repository.test.ts
@@ -1,4 +1,4 @@
-import { DuplicateIdError, NotFoundError } from "../../src/ports/users-management/Errors.js"
+import { DuplicateIdError, NotFoundError, UniquenessConstraintViolatedError } from "../../src/ports/Repository.js"
 
 test("DuplicateIdError cause add correcly", () => {
   const cause = "Device id already taken"
@@ -11,3 +11,51 @@ test("NotFoundError cause add correcly", () => {
   const err = NotFoundError(cause)
   expect(err.cause).toBe(cause)
 })
+
+test("DuplicateIdError has a message", () => {
+    const error = DuplicateIdError();
+    expect(error.message).toBe("Id already in use")
+    expect(error.cause).toBeUndefined()
+});
+
+test("DuplicateIdError has a cause", () => {
+    const error = DuplicateIdError("This is the cause");
+    expect(error.cause).toBe("This is the cause")
+});
+
+test("DuplicateIdError has a brand", () => {
+    const error = DuplicateIdError();
+    expect(error.__brand).toBe("DuplicateIdError");
+});
+
+test("NotFoundError has a message", () => {
+    const error = NotFoundError();
+    expect(error.message).toBe("Not found")
+    expect(error.cause).toBeUndefined()
+});
+
+test("NotFoundError has a cause", () => {
+    const error = NotFoundError("This is the cause");
+    expect(error.cause).toBe("This is the cause")
+});
+
+test("NotFoundError has a brand", () => {
+    const error = NotFoundError();
+    expect(error.__brand).toBe("NotFoundError");
+});
+
+test("UniquenessConstraintViolatedError has a message", () => {
+    const error = UniquenessConstraintViolatedError();
+    expect(error.message).toBe("A uniqueness constraint was violated")
+    expect(error.cause).toBeUndefined()
+});
+
+test("UniquenessConstraintViolatedError has a cause", () => {
+    const error = UniquenessConstraintViolatedError("This is the cause");
+    expect(error.cause).toBe("This is the cause")
+});
+
+test("UniquenessConstraintViolatedError has a brand", () => {
+    const error = UniquenessConstraintViolatedError();
+    expect(error.__brand).toBe("UniquenessConstraintViolatedError");
+});
