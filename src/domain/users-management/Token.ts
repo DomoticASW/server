bb8822bba496a327a1b1c7a2f061b5dc7d127ff5
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-04-18 17:52:57 +0200

    refactor: update Token interface to use Role type

--- src/domain/users-management/Token.ts
+++ src/domain/users-management/Token.ts
@@ -1,24 +1,19 @@
-import { Email } from "./User.js";
-
-export enum UserRole {
-    Admin = "Admin",
-    User = "User"
-}
+import { Email, Role } from "./User.js";
 
 export interface Token {
     readonly userEmail: Email;
-    readonly role: UserRole;
-    readonly source: string
+    readonly role: Role;
+    readonly source: string;
 }
 
-export function Token(userEmail: Email, role: UserRole, source: string): Token {
+export function Token(userEmail: Email, role: Role, source: string): Token {
     return new TokenImpl(userEmail, role, source);
 }
 
 class TokenImpl implements Token {    
     constructor(
         public readonly userEmail: Email,
-        public readonly role: UserRole,
-        public readonly source: string
+        public readonly role: Role,
+        public readonly source: string,
     ) {}
 }
