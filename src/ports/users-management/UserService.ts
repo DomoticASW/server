94d2df335c0373baf0371e80eaa7391ede3537b9
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-04-18 17:53:58 +0200

    feat: add JWT token handling in UsersService

--- src/ports/users-management/UserService.ts
+++ src/ports/users-management/UserService.ts
@@ -1,9 +1,11 @@
 import { Effect } from "effect/Effect";
 import { User, Nickname, Email, PasswordHash, Role, UserImpl } from "../../domain/users-management/User.js";
 import { Token } from "../../domain/users-management/Token.js";
-import { EmailAlreadyInUseError, UserNotFoundError, TokenError, InvalidTokenError, InvalidCredentialsError, InvalidTokenFormatError, userNotFoundError } from "./Errors.js";
+import { EmailAlreadyInUseError, UserNotFoundError, TokenError, InvalidTokenError, InvalidCredentialsError, InvalidTokenFormatError, } from "./Errors.js";
 import { UserRepository } from "./UserRepository.js";
 import { Effect as Eff } from "effect";
+import { pipe } from "effect";
+import jwt from 'jsonwebtoken';
 
 export interface UsersService {
     publishRegistrationRequest(nickname: Nickname, email: Email, password: PasswordHash): Effect<void, EmailAlreadyInUseError>;
@@ -19,17 +21,60 @@ export interface UsersService {
 }
 
 export class UsersServiceImpl implements UsersService {
+    
     constructor(
         private userRepository: UserRepository,
-        private adminEmail: Email
+        private adminEmail: Email,
+        private secret: string,
     ) { }
-
+    
     publishRegistrationRequest(
         nickname: Nickname,
         email: Email,
         password: PasswordHash,
     ): Effect<void, EmailAlreadyInUseError> {
-        const newUser = new UserImpl(nickname, email, password, Role.User);
+        const newUser = User(nickname, email, password, Role.User);
         return this.userRepository.add(newUser).pipe(Eff.mapError(() => EmailAlreadyInUseError()));
+    }
+    
+    getAllUsers(token: Token): Effect<Iterable<User>, InvalidTokenError> {
+        return this.verifyToken(token).pipe(
+            Eff.flatMap(() => this.userRepository.getAll())
+        );
+    }
+    
+    login(email: Email, password: PasswordHash): Effect<Token, InvalidCredentialsError> {
+        return pipe(
+            this.userRepository.find(email),
+            Eff.flatMap(user => {
+                if (user.passwordHash !== password) {
+                    return Eff.fail(InvalidCredentialsError())
+                }
+                const source = jwt.sign({ email: user.email, role: user.role}, this.secret, { expiresIn: '1h' });
+                return Eff.succeed(Token(user.email, user.role, source));
+            }),
+            Eff.mapError(() => InvalidCredentialsError())
+        );
+    }
+    
+    verifyToken(token: Token): Effect<void, InvalidTokenError> {
+        if (jwt.verify(token.source, this.secret)) {
+            return Eff.succeed(null);
+        }
+        return Eff.fail(InvalidTokenError())
+    }
+    
+    makeToken(value: string): Effect<Token, InvalidTokenFormatError> {
+        return pipe(
+            Eff.try({
+                try: () => jwt.decode(value) as {email: string, role: Role},
+                catch: () => InvalidTokenFormatError(),
+            }),
+            Eff.flatMap((decoded) => 
+                decoded
+                ? Eff.succeed(Token(Email(decoded.email), decoded.role, value))
+                : Eff.fail(InvalidTokenFormatError())
+            )
+        );
     }
 }
