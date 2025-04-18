8d6dc4bee6a6d296f332cbbc024cec9a25a639ce
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-04-18 08:54:50 +0200

    refactor: remove unused test and update Token interface to include source

--- src/domain/users-management/Token.ts
+++ src/domain/users-management/Token.ts
@@ -1,7 +1,4 @@
 import { Email } from "./User.js";
-import { InvalidTokenFormatError } from "../../ports/users-management/Errors.js";
-import { Brand } from "../../utils/Brand.js";
-import jwt from 'jsonwebtoken';
 
 export enum UserRole {
     Admin = "Admin",
@@ -11,36 +8,17 @@ export enum UserRole {
 export interface Token {
     readonly userEmail: Email;
     readonly role: UserRole;
-    
-    newToken(string: string): Brand<string, "Token" | InvalidTokenFormatError>;
+    readonly source: string
 }
 
-export function Token(userEmail: Email, role: UserRole): Token {
-    return new TokenImpl(userEmail, role);
+export function Token(userEmail: Email, role: UserRole, source: string): Token {
+    return new TokenImpl(userEmail, role, source);
 }
 
-class TokenImpl implements Token {
-    private static SECRET_KEY = process.env.JWT_SECRET_KEY || 'default-secret-key';
-    
+class TokenImpl implements Token {    
     constructor(
         public readonly userEmail: Email,
-        public readonly role: UserRole
+        public readonly role: UserRole,
+        public readonly source: string
     ) {}
-    
-    newToken(value: string): Brand<string, "Token" | InvalidTokenFormatError> {
-        try {
-            const decoded = jwt.verify(value, TokenImpl.SECRET_KEY) as {
-                userEmail: string;
-                role: string;
-            };
-            
-            if (!decoded.userEmail || !decoded.role) {
-                return "InvalidTokenFormatError" as Brand<string, InvalidTokenFormatError>;
-            }
-
-            return value as Brand<string, "Token">;
-        } catch (error) {
-            return "InvalidTokenFormatError" as Brand<string, InvalidTokenFormatError>;
-        }
-    }
 }
