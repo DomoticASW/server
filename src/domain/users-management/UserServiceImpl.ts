aa209a41c15b1592f4e7fd8cd68622a546b6a37b
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-04-21 02:13:54 +0200

    feat: add UsersServiceImpl implementation and test

---
+++ src/domain/users-management/UserServiceImpl.ts
@@ -0,0 +1,178 @@
+import { Effect } from "effect/Effect";
+import jwt from 'jsonwebtoken';
+import { User, Nickname, Email, PasswordHash, Role } from "./User.js";
+import { Effect as Eff } from "effect";
+import { pipe } from "effect";
+import { RegistrationRequest } from "./RegistrationRequest.js";
+import { EmailAlreadyInUseError, UserNotFoundError, TokenError, InvalidTokenError, InvalidCredentialsError, InvalidTokenFormatError, UnauthorizedError, } from "../../ports/users-management/Errors.js";
+import { Token } from "./Token.js";
+import { UsersService } from "../../ports/users-management/UserService.js";
+import { UserRepositoryAdapter } from "../../adapters/users-management/UserRepositoryAdapter.js";
+import { RegistrationRequestRepositoryAdapter } from "../../adapters/users-management/RegistrationRequestRepositoryAdapter.js";
+
+export class UsersServiceImpl implements UsersService {
+    
+    constructor(
+        private userRepository: UserRepositoryAdapter,
+        private regReqRepository: RegistrationRequestRepositoryAdapter,
+        private secret: string,
+    ) { }
+    
+    publishRegistrationRequest(
+        nickname: Nickname,
+        email: Email,
+        password: PasswordHash,
+    ): Effect<void, EmailAlreadyInUseError> {
+        const newRegReq = RegistrationRequest(nickname, email, password);
+        return pipe(
+            this.regReqRepository.add(newRegReq),
+            Eff.mapError(() => EmailAlreadyInUseError()
+        ))
+    }
+    
+    approveRegistrationRequest(token: Token, email: Email): Effect<void, UserNotFoundError | TokenError> {
+        return pipe(
+            Eff.if(token.role == Role.Admin, {
+                onTrue: () => this.verifyToken(token),
+                onFalse: () => Eff.fail(UnauthorizedError())
+            }),
+            Eff.flatMap(() => this.regReqRepository.find(email)),
+            Eff.flatMap((regReq) => {
+                const newUser = User(regReq.nickname, regReq.email, regReq.passwordHash, Role.User);
+                return this.userRepository.add(newUser)
+            }),
+            Eff.mapError(e => {
+                switch (e.__brand) {
+                    case "NotFoundError":
+                        return UserNotFoundError(e.cause)
+                    case "UnauthorizedError":
+                        return UnauthorizedError()
+                    default:
+                        return InvalidTokenError()
+                }
+            }),
+        )
+    }
+    
+    rejectRegistrationRequest(token: Token, email: Email): Effect<void, UserNotFoundError | TokenError> {
+        return pipe(
+            Eff.if(token.role == Role.Admin, {
+                onTrue: () => this.verifyToken(token),
+                onFalse: () => Eff.fail(UnauthorizedError())
+            }),
+            Eff.flatMap(() => this.regReqRepository.remove(email)),
+            Eff.mapError(e => {
+                switch (e.__brand) {
+                    case "NotFoundError":
+                        return UserNotFoundError(e.cause)
+                    default:
+                        return e
+                }
+            }),
+        )
+    }
+
+    removeUser(token: Token, email: Email): Effect<void, UserNotFoundError | TokenError> {
+        return pipe(
+            Eff.if(token.role == Role.Admin, {
+                onTrue: () => this.verifyToken(token),
+                onFalse: () => Eff.fail(UnauthorizedError())
+            }),
+            Eff.flatMap(() => this.userRepository.remove(email)),
+            Eff.mapError(e => {
+                switch (e.__brand) {
+                    case "NotFoundError":
+                        return UserNotFoundError(e.cause)
+                    default:
+                        return e
+                }
+            }),
+        )
+    }
+
+    // ??? email, emailAlreadyInUseError
+    updateUserData(
+        token: Token,
+        email: Email,
+        nickname?: Nickname,
+        password?: PasswordHash
+    ): Effect<void, UserNotFoundError | EmailAlreadyInUseError | TokenError> {
+        return pipe(
+            Eff.if(token.role == Role.Admin, {
+                onTrue: () => this.verifyToken(token),
+                onFalse: () => Eff.fail(UnauthorizedError())
+            }),
+            Eff.flatMap(() => this.userRepository.find(email)),
+            Eff.flatMap((user) => {
+                const updatedUser = User(nickname ?? user.nickname, email, password ?? user.passwordHash, user.role);
+                return this.userRepository.update(updatedUser)
+            }),
+            Eff.mapError(e => {
+                switch (e.__brand) {
+                    case "NotFoundError":
+                        return UserNotFoundError(e.cause)
+                    default:
+                        return e
+                }
+            }),
+        )
+    }
+    
+    getAllUsers(token: Token): Effect<Iterable<User>, InvalidTokenError> {
+        return pipe(
+            Eff.if(token.role == Role.Admin, {
+                onTrue: () => this.verifyToken(token),
+                onFalse: () => Eff.fail(UnauthorizedError())
+            }),
+            Eff.flatMap(() => this.userRepository.getAll()),
+            Eff.mapError(() => InvalidTokenError())
+        );
+    }
+
+    // ??? find
+    getUserData(token: Token): Effect<User, InvalidTokenError> {
+        return pipe(
+            Eff.if(token.role == Role.Admin, {
+                onTrue: () => this.verifyToken(token),
+                onFalse: () => Eff.fail(UnauthorizedError())
+            }),
+            Eff.flatMap(() => this.userRepository.find(token.userEmail)),
+            Eff.mapError(() => InvalidTokenError())
+        );
+    }
+    
+    login(email: Email, password: PasswordHash): Effect<Token, InvalidCredentialsError> {
+        return pipe(
+            this.userRepository.find(email),
+            Eff.flatMap(user => {
+                if (user.passwordHash !== password) {
+                    return Eff.fail(InvalidCredentialsError())
+                }
+                const source = jwt.sign({ email: user.email, role: user.role}, this.secret, { expiresIn: '1h' });
+                return Eff.succeed(Token(user.email, user.role, source));
+            }),
+            Eff.mapError(() => InvalidCredentialsError())
+        );
+    }
+    
+    verifyToken(token: Token): Effect<void, InvalidTokenError> {
+        if (jwt.verify(token.source, this.secret)) {
+            return Eff.succeed(null);
+        }
+        return Eff.fail(InvalidTokenError())
+    }
+    
+    makeToken(value: string): Effect<Token, InvalidTokenFormatError> {
+        return pipe(
+            Eff.try({
+                try: () => jwt.decode(value) as {email: string, role: Role},
+                catch: () => InvalidTokenFormatError(),
+            }),
+            Eff.flatMap((decoded) => 
+                decoded
+                ? Eff.succeed(Token(Email(decoded.email), decoded.role, value))
+                : Eff.fail(InvalidTokenFormatError())
+            )
+        );
+    }
+}
