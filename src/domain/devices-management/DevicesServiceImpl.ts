57889fa71ea9739be821a59b9eecc1dc6ea3e6e3
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-04-22 10:57:39 +0200

    feat: implement DevicesService.find/remove

--- src/domain/devices-management/DevicesServiceImpl.ts
+++ src/domain/devices-management/DevicesServiceImpl.ts
@@ -1,4 +1,4 @@
-import { Effect, Match } from "effect";
+import { Effect, pipe } from "effect";
 import { DevicePropertyUpdatesSubscriber, DevicesService } from "../../ports/devices-management/DevicesService.js";
 import { DeviceUnreachableError, DeviceNotFoundError, InvalidInputError, DeviceActionError, DeviceActionNotFound, DevicePropertyNotFound, DeviceAlreadyRegisteredError } from "../../ports/devices-management/Errors.js";
 import { PermissionError } from "../../ports/permissions-management/Errors.js";
@@ -35,13 +35,31 @@ export class DevicesServiceImpl implements DevicesService {
         )
     }
     remove(token: Token, deviceId: DeviceId): Effect.Effect<void, DeviceNotFoundError | TokenError> {
-        throw new Error("Method not implemented.");
+        // TODO: check token
+        return pipe(
+            this.repo.remove(deviceId),
+            Effect.mapError((e) => {
+                switch (e.__brand) {
+                    case "NotFoundError":
+                        return DeviceNotFoundError(e.cause)
+                }
+            })
+        )
     }
     rename(token: Token, deviceId: DeviceId, name: string): Effect.Effect<void, DeviceNotFoundError | TokenError> {
         throw new Error("Method not implemented.");
     }
     find(token: Token, deviceId: DeviceId): Effect.Effect<Device, DeviceNotFoundError | InvalidTokenError> {
-        throw new Error("Method not implemented.");
+        // TODO: check token
+        return pipe(
+            this.repo.find(deviceId),
+            Effect.mapError((e) => {
+                switch (e.__brand) {
+                    case "NotFoundError":
+                        return DeviceNotFoundError(e.cause)
+                }
+            })
+        )
     }
     getAllDevices(token: Token): Effect.Effect<Iterable<Device>, InvalidTokenError> {
         // TODO: check token
