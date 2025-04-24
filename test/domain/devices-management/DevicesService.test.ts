380104aec558cf7ebb07f743b81001dfd731bad8
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-04-24 11:11:38 +0200

    feat: impl. DevicesService.updateDeviceProperty

--- test/domain/devices-management/DevicesService.test.ts
+++ test/domain/devices-management/DevicesService.test.ts
@@ -1,5 +1,5 @@
 import { Effect, pipe } from "effect"
-import { DeviceId, Device, DeviceStatus } from "../../../src/domain/devices-management/Device.js"
+import { DeviceId, Device, DeviceStatus, DevicePropertyId, DeviceProperty, DeviceAction, DeviceEvent } from "../../../src/domain/devices-management/Device.js"
 import { UserRole, Token } from "../../../src/domain/users-management/Token.js"
 import { Email } from "../../../src/domain/users-management/User.js"
 import { DevicesService } from "../../../src/ports/devices-management/DevicesService.js"
@@ -8,6 +8,7 @@ import { DevicesServiceImpl } from "../../../src/domain/devices-management/Devic
 import { DeviceFactory } from "../../../src/ports/devices-management/DeviceFactory.js"
 import { DeviceUnreachableError } from "../../../src/ports/devices-management/Errors.js"
 import { DeviceRepository } from "../../../src/ports/devices-management/DeviceRepository.js"
+import { NoneInt } from "../../../src/domain/devices-management/Types.js"
 
 let service: DevicesService
 let deviceFactory: DeviceFactory
@@ -24,7 +25,10 @@ beforeEach(() => {
     repo = new InMemoryRepositoryMock((d) => d.id)
     deviceFactory = {
         create(deviceUrl: URL): Effect.Effect<Device, DeviceUnreachableError> {
-            return Effect.succeed(Device(DeviceId(deviceUrl.toString()), "device", deviceUrl, DeviceStatus.Online, [], [], []))
+            const properties = [DeviceProperty(DevicePropertyId("prop"), "prop", 0, NoneInt())]
+            const actions: DeviceAction<unknown>[] = []
+            const events: DeviceEvent[] = []
+            return Effect.succeed(Device(DeviceId(deviceUrl.toString()), "device", deviceUrl, DeviceStatus.Online, properties, actions, events))
         }
     }
     service = new DevicesServiceImpl(repo, deviceFactory)
@@ -126,4 +130,41 @@ test("rename returns an error in case device is not found", () => {
         service.rename(makeToken(), DeviceId(new URL("http://localhost").toString()), "newName"),
         Effect.runSync
     )).toThrow("DeviceNotFoundError")
+})
+
+test("updateDeviceProperty updates the value of a device property", () => {
+    pipe(
+        Effect.gen(function* () {
+            const id = yield* service.add(makeToken(), new URL("http://localhost"))
+            const device = yield* service.find(makeToken(), id)
+            const property = device.properties[0]
+            const oldValue = property.value
+            const newValue = 12098
+            expect(oldValue).not.toEqual(newValue)
+            yield* service.updateDeviceProperty(id, property.id, newValue)
+            const newDevice = yield* service.find(makeToken(), id)
+            const newProperty = newDevice.properties[0]
+            expect(newProperty.value).toEqual(newValue)
+        }),
+        Effect.runSync
+    )
+})
+
+test("updateDeviceProperty returns an error in case device is not found", () => {
+    const deviceId = DeviceId(new URL("http://localhost").toString())
+    const propertyId = DevicePropertyId("property")
+    expect(() => pipe(
+        service.updateDeviceProperty(deviceId, propertyId, 3),
+        Effect.runSync
+    )).toThrow("DeviceNotFoundError")
+})
+
+test("updateDeviceProperty returns an error in case device property is not found", () => {
+    expect(() => pipe(
+        Effect.gen(function* () {
+            const id = yield* service.add(makeToken(), new URL("http://localhost"))
+            yield* service.updateDeviceProperty(id, DevicePropertyId("property"), 3)
+        }),
+        Effect.runSync
+    )).toThrow("DevicePropertyNotFound")
 })
