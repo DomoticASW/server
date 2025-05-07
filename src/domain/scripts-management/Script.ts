ae4f437311d06aaa1c16804546b22f7e85c17b63
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-05-07 09:42:37 +0200

    feat: add creation and execution of tasks

--- src/domain/scripts-management/Script.ts
+++ src/domain/scripts-management/Script.ts
@@ -1,9 +1,10 @@
-import { Effect } from "effect/Effect"
+import { Effect, reduce } from "effect/Effect"
 import { ScriptError } from "../../ports/scripts-management/Errors.js"
 import { Brand } from "../../utils/Brand.js"
 import { ExecutionEnvironment, Instruction } from "./Instruction.js"
 import { Trigger } from "./Trigger.js"
 import { Token } from "../users-management/Token.js"
+import { pipe } from "effect"
 
 export interface Script<Id extends ScriptId> {
   readonly id: Id
@@ -29,17 +30,15 @@ export type AutomationId = Brand<string, "AutomationId">
 export function TaskId(id: string): TaskId { return id as TaskId }
 export function AutomationId(id: string): AutomationId { return id as AutomationId }
 
-// export function Task(id: TaskId, name: string, instructions: Array<Instruction>): Task {
-//   return {
-//     id: id,
-//     name: name,
-//     instructions: instructions,
-//     execute() {
-//       // let env = ExecutionEnvironment()
-
-//       // this.instructions.forEach(instruction =>
-//       //   env = instruction.execute(env)
-//       // )
-//     },
-//   }
-// }
+export function Task(id: TaskId, name: string, instructions: Array<Instruction>): Task {
+  return {
+    id: id,
+    name: name,
+    instructions: instructions,
+    execute(token) {
+      return pipe(
+        reduce(this.instructions, ExecutionEnvironment(token), (env, instr) => instr.execute(env))
+      )
+    },
+  }
+}
--- test/domain/scripts-management/Script.test.ts
+++ test/domain/scripts-management/Script.test.ts
@@ -1,7 +1,11 @@
-// import { CreateConstantInstruction } from "../../../src/domain/scripts-management/InstructionImpl.js"
-import { AutomationId, TaskId } from "../../../src/domain/scripts-management/Script.js"
+import { match, runPromise } from "effect/Effect"
+import { CreateConstantInstruction, StartTaskInstruction } from "../../../src/domain/scripts-management/InstructionImpl.js"
+import { AutomationId, Task, TaskId } from "../../../src/domain/scripts-management/Script.js"
+import { Type } from "../../../src/ports/devices-management/Types.js"
 import { ScriptError, ScriptNotFoundError } from "../../../src/ports/scripts-management/Errors.js"
-// import { Type } from "../../../src/ports/devices-management/Types.js"
+import { PermissionsServiceSpy, ScriptsServiceSpy, SpyTaskMock, TokenMock } from "./mocks.js"
+import { pipe } from "effect"
+import { PermissionError } from "../../../src/ports/permissions-management/Errors.js"
 
 test("A script error can be created", () => {
   const error = ScriptError("this is the cause")
@@ -27,24 +31,66 @@ test("An automationId can be created", () => {
   expect(automationId).toBe("1")
 })
 
-// test("A task can be created", () => {
-//   const taskId = TaskId("1")
-//   const task = Task(taskId, "taskName", [])
+test("A task can be created", () => {
+  const taskId = TaskId("1")
+  const task = Task(taskId, "taskName", [])
 
-//   expect(task.id).toBe(taskId)
-//   expect(task.name).toBe("taskName")
-// })
+  expect(task.id).toBe(taskId)
+  expect(task.name).toBe("taskName")
+})
 
-// How to test execution of a task without a spy?
-// test("A task can be executed", () => {
-//   const taskId = TaskId("1")
-//   const instruction1 = CreateConstantInstruction("constantName1", Type.IntType, 15)
-//   const instruction2 = CreateConstantInstruction("constantName2", Type.IntType, 25)
-//   const instructions = [
-//     instruction1,
-//     instruction2
-//   ]
+test("A task can be executed", async () => {
+  const taskId = TaskId("1")
+  const instruction1 = CreateConstantInstruction("constantName1", Type.IntType, 15)
+  const instruction2 = CreateConstantInstruction("constantName2", Type.IntType, 25)
+  const instructions = [
+    instruction1,
+    instruction2
+  ]
 
-//   const task = Task(taskId, "taskName", instructions)
-//   task.execute()
-// })
+  const task = Task(taskId, "taskName", instructions)
+  const env = await runPromise(task.execute(TokenMock("email")))
+
+  expect(env.constants.get(instruction1)).toBeDefined()
+  expect(env.constants.get(instruction2)).toBeDefined()
+})
+
+test("A task cannot execute another task id token has not the permissions", async () => {
+  const spyTask = SpyTaskMock().get()
+  const scriptsService = ScriptsServiceSpy(spyTask, true).get()
+  const requiredToken = TokenMock("email")
+  const permissionsService = PermissionsServiceSpy(requiredToken).get()
+  
+  const startTaskInstruction = StartTaskInstruction(spyTask.id, scriptsService, permissionsService)
+  
+  const taskId = TaskId("1")
+  const task = Task(taskId, "taskName", [startTaskInstruction])
+  
+  await pipe(
+    task.execute(TokenMock("otherEmail")),
+    match({
+      onSuccess() { throw Error("Should not be here") },
+      onFailure(err) {
+        expect(err.__brand).toBe("ScriptError")
+        expect(err.cause).toBe(PermissionError().message + ", " + PermissionError().cause)
+      }
+    }),
+    runPromise
+  )
+})
+
+test("A task can be started by another task if the token has the permissions", async () => {
+  const spyTask = SpyTaskMock()
+  const scriptsService = ScriptsServiceSpy(spyTask.get(), true).get()
+  const requiredToken = TokenMock("email")
+  const permissionsService = PermissionsServiceSpy(requiredToken).get()
+  
+  const startTaskInstruction = StartTaskInstruction(spyTask.get().id, scriptsService, permissionsService)
+
+  const taskId = TaskId("1")
+  const task = Task(taskId, "taskName", [startTaskInstruction])
+
+  await runPromise(task.execute(requiredToken))
+
+  expect(spyTask.call()).toBe(1)
+})
