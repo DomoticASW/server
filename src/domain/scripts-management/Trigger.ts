52f5eff2fce4682453cf4d2edd0716879d051395
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-05-07 09:55:56 +0200

    chore: add creation of triggers

--- src/domain/scripts-management/Trigger.ts
+++ src/domain/scripts-management/Trigger.ts
@@ -11,3 +11,17 @@ export interface PeriodTrigger {
   start: Date
   periodSeconds: number
 }
+
+export function DeviceEventTrigger(deviceId: DeviceId, eventName: string): DeviceEventTrigger {
+  return {
+    deviceId: deviceId,
+    eventName: eventName
+  }
+}
+
+export function PeriodTrigger(start: Date, periodSeconds: number): PeriodTrigger {
+  return {
+    start: start,
+    periodSeconds: periodSeconds
+  }
+}
