398f1e8022c5e176a2b63a83038e54d6d8c2c666
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-05-17 14:06:21 +0200

    chore: removed some comments

--- src/domain/notifications-management/NotificationsServiceImpl.ts
+++ src/domain/notifications-management/NotificationsServiceImpl.ts
@@ -23,55 +23,6 @@ class NotificationsServiceImpl implements NotificationsService {
     this.notificationProtocol = notificationProtocol
   }
 
-  // Change verifyToken to getUserUnsafe
-
-  // private setupSocketHandling() {
-  //   this.io.on("connection", (socket: Socket) => {
-  //     // The idea is that as soon the user logs in, it can start receiving notifications from tasks/automations 
-  //     // or from offline devices if specified
-  //     // Every time the user logs in, all their deviceOfflineNotificationSubscription will be reloaded
-  //     socket.on("login", async ({ email }: { email: Email }) => {
-  //       await runPromise(pipe(
-  //         this.usersService.getUserDataUnsafe(email),
-  //         map(() => socket.data.email = email),
-  //         flatMap(() => this.loadSubscriptionsFromRepository(email, socket)),
-  //         mapError(err => socket.emit(err.__brand, err.message + ": " + err.cause))
-  //       ));
-  //     })
-
-  //     socket.on("disconnect", () => {
-  //       const email = socket.data?.email
-  //       if (email) {
-  //         this.cleanupSocket(email, socket)
-  //       }
-  //     })
-  //   })
-  // }
-
-  // private loadSubscriptionsFromRepository(email: Email, socket: Socket): Effect<void> {
-  //   return pipe(
-  //     this.subscriptionsRepository.getAll(),
-  //     map(subscriptions => {
-  //       for (const sub of subscriptions) {
-  //         if (sub.email === email) {
-  //           this.registrySocket(sub.deviceId, email, socket)
-  //         }
-  //       }
-  //     })
-  //   );
-  // }
-
-  // private cleanupSocket(email: Email, socket: Socket) {
-  //   for (const [deviceId, subscribers] of this.deviceSubscriptions.entries()) {
-  //     if (subscribers.get(email) === socket) {
-  //       subscribers.delete(email);
-  //       if (subscribers.size === 0) {
-  //         this.deviceSubscriptions.delete(deviceId);
-  //       }
-  //     }
-  //   }
-  // }
-
   subscribeForDeviceOfflineNotifications(email: Email, deviceId: DeviceId): Effect<void, DeviceNotFoundError | UserNotFoundError> {
     return pipe(
       this.devicesService.findUnsafe(deviceId),
@@ -139,16 +90,6 @@ class NotificationsServiceImpl implements NotificationsService {
       })
     );
   }
-  // private findSocketByEmail(email: Email): Effect<Socket, UserNotFoundError> {
-  //   const sockets = this.io.sockets.sockets.values()
-  //   for (const socket of sockets) {
-  //     if (socket.data.email === email) {
-  //       return succeed(socket)
-  //     }
-  //   }
-
-  //   return fail(UserNotFoundErrorMock()) //Add real InvalidTokenError
-  // }
 }
 
 export function NotificationsService(deviceStatusesService: DeviceStatusesService, devicesService: DevicesService, usersService: UsersService, subscriptionsRepository: DeviceOfflineNotificationSubscriptionRepository) : NotificationsService {
