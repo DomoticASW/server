6f568ba420dc894ae0a12821d77f9211a415d375
Alessandro Venturini <ales.ventus21@gmail.com>
2025-05-18 11:11:59 +0200

    feat: now ScriptRepository fully stores Scripts

--- test/adapters/scripts-management/ScriptRepositoryMongoAdapter.test.ts
+++ test/adapters/scripts-management/ScriptRepositoryMongoAdapter.test.ts
@@ -3,7 +3,12 @@ import { testRepositoryMongoAdapter } from "../BaseRepositoryMongoAdapterTests.j
 import { ScriptRepositoryMongoAdapter } from "../../../src/adapters/scripts-management/ScriptRepositoryMongoAdapter.js";
 import { ScriptId, AutomationId, TaskId, Task, Automation } from "../../../src/domain/scripts-management/Script.js";
 import { DeviceEventTrigger, PeriodTrigger, Trigger } from "../../../src/domain/scripts-management/Trigger.js";
-import { DeviceId } from "../../../src/domain/devices-management/Device.js";
+import { DeviceActionId, DeviceId, DevicePropertyId } from "../../../src/domain/devices-management/Device.js";
+import { CreateConstantInstruction, CreateDevicePropertyConstantInstruction, DeviceActionInstruction, ElseInstruction, IfInstruction, SendNotificationInstruction, StartTaskInstruction, WaitInstruction } from "../../../src/domain/scripts-management/InstructionImpl.js";
+import { Type } from "../../../src/ports/devices-management/Types.js";
+import { Email } from "../../../src/domain/users-management/User.js";
+import { BooleanEOperator, ColorEOperator, NumberEOperator, NumberGEOperator, NumberGOperator, NumberLEOperator, NumberLOperator, StringEOperator } from "../../../src/domain/scripts-management/Operators.js";
+import { Condition, ConstantInstruction } from "../../../src/domain/scripts-management/Instruction.js";
 
 const dbName = "ScriptRepositoryMongoAdapterTests"
 
@@ -16,8 +21,35 @@ function makeAutomationId(id: string): AutomationId {
     return AutomationId(id)
 }
 
+const instr1 = WaitInstruction(1)
+const instr2 = WaitInstruction(2)
+const constantA = CreateConstantInstruction("a", Type.IntType, 3) as ConstantInstruction<unknown>
+const constantB = CreateConstantInstruction("b", Type.IntType, 2) as ConstantInstruction<unknown>
+
+const instructions = [
+    SendNotificationInstruction(Email("hello@email.com"), "message"),
+    WaitInstruction(1),
+    StartTaskInstruction(TaskId("1")),
+    DeviceActionInstruction(DeviceId("1"), DeviceActionId("1"), 4),
+    CreateConstantInstruction("x", Type.DoubleType, 3.14),
+    CreateDevicePropertyConstantInstruction("y", Type.IntType, DeviceId("1"), DevicePropertyId("1")),
+    instr1,
+    instr2,
+    constantA,
+    constantB,
+    IfInstruction([instr1, instr2], Condition(constantA, constantB, NumberEOperator())),
+    ElseInstruction([instr1], [instr2], Condition(constantA, constantB, NumberGEOperator(), true)),
+    // All other condition operators
+    IfInstruction([], Condition(constantA, constantB, NumberLEOperator())),
+    IfInstruction([], Condition(constantA, constantB, NumberLOperator())),
+    IfInstruction([], Condition(constantA, constantB, NumberGOperator())),
+    IfInstruction([], Condition(constantA, constantB, StringEOperator())),
+    IfInstruction([], Condition(constantA, constantB, ColorEOperator())),
+    IfInstruction([], Condition(constantA, constantB, BooleanEOperator()))
+]
+
 function makeTaskEntity(id: string = "1", something: string = "Turn lights off"): Task {
-    return Task(TaskId(id), something, [])
+    return Task(TaskId(id), something, instructions)
 }
 function makeAutomationEntity(id: string = "1", something: string = "a"): Automation {
     let trigger: Trigger
@@ -26,7 +58,7 @@ function makeAutomationEntity(id: string = "1", something: string = "a"): Automa
     } else {
         trigger = DeviceEventTrigger(DeviceId("1"), "event")
     }
-    return Automation(AutomationId(id), something, trigger, [])
+    return Automation(AutomationId(id), something, trigger, instructions)
 }
 
 function makeRepository(connection: mongoose.Connection): ScriptRepositoryMongoAdapter {
