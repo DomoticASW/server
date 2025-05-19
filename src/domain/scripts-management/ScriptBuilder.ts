19aa29fcc9ac833f66885c88619fdefd2762f197
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-05-19 15:05:04 +0200

    chore: add of wait instruction with task Builder

--- src/domain/scripts-management/ScriptBuilder.ts
+++ src/domain/scripts-management/ScriptBuilder.ts
@@ -1,19 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Effect, succeed } from "effect/Effect";
 import { DeviceActionId, DeviceId, DevicePropertyId } from "../devices-management/Device.js";
-import { ConditionOperator } from "./Instruction.js";
-import { ConstantRef, ElseNodeRef, NodeRef, ThenNodeRef } from "./Refs.js";
+import { ConditionOperator, Instruction } from "./Instruction.js";
+import { ConstantRef, ElseNodeRef, NodeRef, RootNodeRef, ThenNodeRef } from "./Refs.js";
 import { Automation, Script, ScriptId, Task, TaskId } from "./Script.js";
 import { Email } from "../users-management/User.js";
 import { Type } from "../../ports/devices-management/Types.js";
 import { InvalidScriptError } from "../../ports/scripts-management/Errors.js";
 import { Trigger } from "./Trigger.js";
 import * as uuid from "uuid";
+import { WaitInstruction } from "./InstructionImpl.js";
 
 interface ScriptBuilder<S = Task | Automation> {
   addIf<T>(ref: NodeRef, left: ConstantRef, right: ConstantRef, operator: ConditionOperator<T>, negate: boolean): [ScriptBuilder<S>, ThenNodeRef];
   addIfElse<T>(ref: NodeRef, left: ConstantRef, right: ConstantRef, operator: ConditionOperator<T>, negate: boolean): [ScriptBuilder<S>, ThenNodeRef, ElseNodeRef];
-  addWait(ref: string, time: number): ScriptBuilder<S>;
+  addWait(ref: NodeRef, seconds: number): ScriptBuilder<S>;
   addSendNotification(ref: NodeRef, email: Email, message: string): ScriptBuilder<S>;
   addDeviceAction(ref: NodeRef, deviceId: DeviceId, actionId: DeviceActionId, input: object): ScriptBuilder<S>;
   addStartTask(ref: NodeRef, taskId: TaskId): ScriptBuilder<S>;
@@ -27,8 +28,14 @@ export type AutomationBuilder = ScriptBuilder<Automation>
 export type TaskBuilder = ScriptBuilder<Task>
 
 abstract class ScriptBuilderImpl<S = Task | Automation> implements ScriptBuilder<S> {
-  constructor(protected name: string) {
+  protected instructions: Array<Instruction> = []
 
+  constructor(protected name: string) {}
+
+  private createCopy(instruction: Instruction): ScriptBuilderImpl<S> {
+    const newBuilder = this.copy()
+    newBuilder.instructions.push(instruction)
+    return newBuilder
   }
 
   addIf<T>(ref: NodeRef, left: ConstantRef, right: ConstantRef, operator: ConditionOperator<T>, negate: boolean): [ScriptBuilder<S>, ThenNodeRef] {
@@ -37,8 +44,8 @@ abstract class ScriptBuilderImpl<S = Task | Automation> implements ScriptBuilder
   addIfElse<T>(ref: NodeRef, left: ConstantRef, right: ConstantRef, operator: ConditionOperator<T>, negate: boolean): [ScriptBuilder<S>, ThenNodeRef, ElseNodeRef] {
     throw new Error("Method not implemented.");
   }
-  addWait(ref: string, time: number): ScriptBuilder<S> {
-    throw new Error("Method not implemented.");
+  addWait(ref: NodeRef, seconds: number): ScriptBuilder<S> {
+    return this.createCopy(WaitInstruction(seconds))
   }
   addSendNotification(ref: NodeRef, email: Email, message: string): ScriptBuilder<S> {
     throw new Error("Method not implemented.");
@@ -56,11 +63,16 @@ abstract class ScriptBuilderImpl<S = Task | Automation> implements ScriptBuilder
     throw new Error("Method not implemented.");
   }
   abstract build(): Effect<S, InvalidScriptError>
+  protected abstract copy(): ScriptBuilderImpl<S>
 }
 
 class TaskBuilderImpl extends ScriptBuilderImpl<Task> {
   build(): Effect<Task, InvalidScriptError> {
-    return succeed(Task(TaskId(uuid.v4()), this.name, []))
+    return succeed(Task(TaskId(uuid.v4()), this.name, this.instructions))
+  }
+
+  protected copy(): TaskBuilderImpl {
+    return new TaskBuilderImpl(this.name)
   }
 }
 
@@ -69,12 +81,15 @@ class AutomationBuilderImpl extends ScriptBuilderImpl<Automation> {
     super(name)
   }
 
-  build(): Effect<Automation, InvalidScriptError, never> {
+  build(): Effect<Automation, InvalidScriptError> {
     throw new Error("Method not implemented.");
   }
 
+  protected copy(): AutomationBuilderImpl {
+    return new AutomationBuilderImpl(this.name, this.trigger)
+  }
 }
 
-export function TaskBuilder(name: string): TaskBuilder {
-  return new TaskBuilderImpl(name)
+export function TaskBuilder(name: string): [TaskBuilder, RootNodeRef] {
+  return [new TaskBuilderImpl(name), RootNodeRef("root")]
 }
--- test/domain/scripts-management/ScriptBuilder.test.ts
+++ test/domain/scripts-management/ScriptBuilder.test.ts
@@ -1,9 +1,32 @@
 import { runPromise } from "effect/Effect"
 import { TaskBuilder } from "../../../src/domain/scripts-management/ScriptBuilder.js"
+import { RootNodeRef } from "../../../src/domain/scripts-management/Refs.js"
+import { DevicesServiceSpy, NotificationsServiceSpy, PermissionsServiceSpy, ScriptsServiceSpy, TokenMock, UserMock } from "../../utils/mocks.js"
+
+const builderAndRoot = TaskBuilder("taskName")
+const taskBuilder: TaskBuilder = builderAndRoot[0]
+const root: RootNodeRef = builderAndRoot[1]
+
+const user = UserMock()
+const token = TokenMock(user.email)
+// const email = user.email
+// const device = DeviceMock()
+// const deviceId = device.id
+const notificationService = NotificationsServiceSpy(user.email).get()
+const devicesService = DevicesServiceSpy().get()
+const scriptsService = ScriptsServiceSpy().get()
+const permissionsService = PermissionsServiceSpy().get()
 
 test("A TaskBuilder can be created", async () => {
-  const taskBuilder = TaskBuilder("taskName")
   const task = await runPromise(taskBuilder.build())
   expect(task.name).toBe("taskName")
 })
 
+test("A wait instruction can be added", async () => {
+  const taskBuilderWait = taskBuilder.addWait(root, 0.5)
+  const task = await runPromise(taskBuilderWait.build())
+  const start = Date.now()
+
+  await runPromise(task.execute(notificationService, scriptsService, permissionsService, devicesService, token))
+  expect(Date.now()).toBeGreaterThan(start + 0.5 * 1000)
+})
