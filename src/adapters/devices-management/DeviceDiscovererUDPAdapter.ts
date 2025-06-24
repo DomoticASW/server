e4c91675de29c9f69b53e40fc031d0f633a8c8ff
Alessandro Venturini <ales.ventus21@gmail.com>
2025-06-24 12:20:36 +0200

    chore: clearing discovery announces for safety

--- src/adapters/devices-management/DeviceDiscovererUDPAdapter.ts
+++ src/adapters/devices-management/DeviceDiscovererUDPAdapter.ts
@@ -25,13 +25,14 @@ export interface Options {
 }
 
 export class DeviceDiscovererUDPAdapter implements DeviceDiscoverer {
-    private receivedMessages: Map<DeviceId, AnnouncedDevice> = new Map()
+    private receivedMessages: Map<string, AnnouncedDevice> = new Map()
+
     constructor(readonly port: number, readonly rememberDiscoveriesForSeconds: number, { logAnnounces = false }: Options = {}) {
         const socket = dgram.createSocket('udp4', (msg, rinfo) => {
             if (logAnnounces) { console.log(`Announce from ${rinfo.address} - ${msg}`) }
             const obj = JSON.parse(JSON.stringify(msg))
             if (isAnnounceMessage(obj)) {
-                this.receivedMessages.set(DeviceId(obj.id), { ...obj, host: rinfo.address, arrivedAt: new Date() })
+                this.receivedMessages.set(obj.id, { ...obj, host: rinfo.address, arrivedAt: new Date() })
             } else {
                 if (logAnnounces) { console.log("Announce message format was invalid") }
             }
@@ -45,6 +46,13 @@ export class DeviceDiscovererUDPAdapter implements DeviceDiscoverer {
 
         socket.bind(port)
         process.on("exit", () => socket.close())
+
+        // Clear old messages every once in a while
+        setInterval(() => {
+            Array.from(this.receivedMessages.values())
+                .filter(m => m.arrivedAt.getTime() >= new Date().getTime() + this.rememberDiscoveriesForSeconds)
+                .forEach(m => this.receivedMessages.delete(m.id))
+        }, rememberDiscoveriesForSeconds * 1000 * 2)
     }
 
     discoveredDevices(): Iterable<DiscoveredDevice> {
